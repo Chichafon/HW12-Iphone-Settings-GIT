1daf15c37ceaf13f85f27b5b50512072beed79cf
chichafon <chichafonchichafon@gmail.com>
2021-12-20 13:45:09 +0400

    Create tableView.

--- HW12 Iphone Settings GIT/Sources/Scenes/Views/ViewController.swift
+++ HW12 Iphone Settings GIT/Sources/Scenes/Views/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private lazy var models = ["One", "Two", "Three"]
+
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .white
+        title = "Настройки"
+        setupHierarchy()
+        setupLayout()
     }
 
+    private func setupHierarchy() {
+        view.addSubview(tableView)
+    }
 
+    private func setupLayout() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
 }
-
